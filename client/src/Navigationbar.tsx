9cdb517115e75ce94030b00498248ecc1c74f882
Jolien Andries <jolien1.andries@gmail.com>
2022-10-24 20:29:40 +0200

    nu is de navbar responsive design

--- client/src/Navigationbar.tsx
+++ client/src/Navigationbar.tsx
@@ -6,13 +6,16 @@ import { NavLink } from'react-router-dom';
 
 function Navigationbar(){
     return (        
-        <Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls='navigationbarscroll'/>
+            <Navbar.Collapse id="navigationbarscroll" data-bs-target="#navigationbarscroll">
             <Nav className="topnav" variant="pills">
                 <NavLink className="pageLink" to="/">Home</NavLink>
                 <NavLink className="pageLink" to="/Map">Map</NavLink>  
                 <NavLink className="pageLink" to="/Attractions">Attractions</NavLink>  
                 <NavLink className="pageLink" to="/Themeparks">Themeparks</NavLink>                    
             </Nav>
+            </Navbar.Collapse>
         </Navbar>
         
     )
