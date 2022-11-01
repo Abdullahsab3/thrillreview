08ded3f6cb7c9d479cefc76f417bc812bd07635f
Jolien Andries <jolien1.andries@gmail.com>
2022-11-01 18:58:59 +0100

    de basis layout is er (containers naast elkaar, die gecentreerd zijn

--- client/src/Attractions.tsx
+++ client/src/Attractions.tsx
@@ -1,6 +1,52 @@
+import { CardGroup } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+
 function Attractions() {
+
+    function TopTenAttractions() {
+
+
+        return(
+            <Card>
+                <Card.Body>
+                    <Card.Title>Top Ten Attractions Rated by our users</Card.Title>
+                    <Card.Text> Ik vermoed dat hier dan de top 10 komt, queryen naar serverside wss </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+
+    function NewestAttractions() {
+        return(
+            <Card>
+                <Card.Body>
+                    <Card.Title>Newest Attractions</Card.Title>
+                    <Card.Text> Ik vermoed dat hier dan de nieuwe komen, queryen naar serverside wss </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+
+    function SearchAttractions() {
+        return(
+            <Card>
+                <Card.Body>
+                    <Card.Title>Search Attractions</Card.Title>
+                    <Card.Text> een zoekding, queryen naar serverside </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+
     return (
+        <div className='ContentOfPage'>
         <h1> attractions</h1>
+        <CardGroup>
+        <TopTenAttractions />
+        <SearchAttractions />
+        <NewestAttractions />
+        </CardGroup>
+        </div>
 
     );
 }
