939693670a85977f1e5b28890a82e4550e5765fc
Mats Van Molle <73903958+matsvanmolle@users.noreply.github.com>
2022-11-06 12:14:59 +0100

    add Attraction functionality added

--- client/src/addAttraction.tsx
+++ client/src/addAttraction.tsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import { Card, Dropdown } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function AddAttraction() {
-
+    const navigate = useNavigate()
+    
+    const [name, setName] = useState("")
+    const [themepark, setThemepark] = useState("")
+    const [opening, setOpening] = useState("")
+    const [builder, setBuilder] = useState("")
+    const [type, setType] = useState("")
+    const [length, setLength] = useState("")
+    const [height, setHeight] = useState("")
+    const [inversions, setInversions] = useState("")
+    const [duration, setDuration] = useState("")
+    
     function handleSubmit() {
-        alert('whoop whoop')
-    }
+        Axios.post("http://localhost:5001/addAttraction", {
+        name: name,
+        themepark: themepark,
+        opening: opening,
+        Builder: builder,
+        type: type,
+        length: length,
+        height: height,
+        inversions: inversions,
+        duration: duration,
+      }).then((response) => {
+        if (response.data.registered) {
+          navigate("/home")
+        }
+      }).catch(function (error) {
+        if(error.response) {
+          //setError(error.response.data.error)
+        }
+      })
+      }
 
     return (
         <div className="ContentOfPage">
@@ -19,19 +51,19 @@ function AddAttraction() {
                     <Form onSubmit={handleSubmit}>
                         <InputGroup>
                             <Form.Label> name* </Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => setName(e.target.value)}/>
                         </InputGroup>
                         <Form.Group>
                             <Form.Label>theme park*</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => setThemepark(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Opening</Form.Label>
-                            <Form.Control type="date"/>
+                            <Form.Control type="date" onChange={(e) => setOpening(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Builder</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => setBuilder(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>type</Form.Label>
@@ -40,25 +72,26 @@ function AddAttraction() {
                             <InputGroup><Form.Check label="wooden coaster"/> </InputGroup>
                             <InputGroup><Form.Check label="standing coaster"/> </InputGroup>
                             <InputGroup><Form.Check label="sitdown coaster"/> </InputGroup>
-                            <InputGroup><Form.Check label="launch coaster"/> </InputGroup>           
+                            <InputGroup><Form.Check label="launch coaster"/> </InputGroup> 
+
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>length (zal nog gevalideerd worden dat echt cijfer is)</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => setLength(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>height (zal nog gevalideerd worden dat echt cijfer is)</Form.Label> /** https://codesandbox.io/s/9zjo1lp86w?file=/src/Components/InputDecimal.jsx */
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={(e) => setHeight(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>inversions </Form.Label>
-                            <Form.Control type="number" min="0"/>
+                            <Form.Control type="number" min="0" onChange={(e) => setInversions(e.target.value)}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Duration (zal nog gevalideerd worden dat echt cijfer is)</Form.Label>
-                            <Form.Control type="duration" placeholder="00m00s"/>
+                            <Form.Control type="duration" onChange={(e) => setDuration(e.target.value)} placeholder="00m00s"/>
                         </Form.Group>
-                        <Button type="submit">Submit</Button>
+                        <Button onClick={handleSubmit}>Submit</Button>
                     </Form>
                 </Card.Body>
             </Card>
