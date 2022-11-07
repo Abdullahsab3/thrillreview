9251713ce3b196aec8a8454fca734c4b6f18e5b6
Jolien Andries <jolien1.andries@gmail.com>
2022-11-07 19:28:25 +0100

    als je location oke zegt, toont het de kaart op de plaats waar je bent (geen marker of zo om te tonen, kan da op zich wel toevoegen?)

--- client/src/Map.tsx
+++ client/src/Map.tsx
@@ -1,21 +1,30 @@
 import 'leaflet/dist/leaflet.css';
-import {Popup, MapContainer, TileLayer, Marker} from 'react-leaflet';
+import {Popup, MapContainer, TileLayer, Marker, useMap} from 'react-leaflet';
 import './map.css';
 import L from 'leaflet';
 import {Helmet} from 'react-helmet-async';
+import { useState, useEffect } from 'react';
 
+
+function SetToUserLocation() {
+  var map = useMap();
+  map.locate({setView: true, maxZoom: 11});
+  return null;
+}
 
 const LeafletMap = () => {
-    return(
-        
-    <MapContainer id="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+
+  return(
+  <MapContainer id="map" center={[51.505, -0.09]} zoom={11} scrollWheelZoom={false}>
             
        <TileLayer
            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
        />
-    </MapContainer>
-    )
+       <SetToUserLocation />
+  </MapContainer>
+    
+  )
 }
 
 
