4878f23e41fbab86f98016575f0e9b390b41dec0
Abdullah Sabaa Allil <abdallah-sab3@hotmail.com>
2022-11-20 16:58:53 +0100

    second iteration of the attraction page

--- client/src/attractions/attractionPage.tsx
+++ client/src/attractions/attractionPage.tsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { Attraction } from "./Attraction"
 import { backendServer } from "../helpers"
+import { Card, Table } from "react-bootstrap"
+import "./styling/attractionPage.css"
 
 export default function AttractionPage() {
     const [attraction, setAttraction] = useState<Attraction>()
@@ -21,12 +23,55 @@ export default function AttractionPage() {
             setAttraction(new Attraction(name, themepark, openingdate, builder, type, height, length, inversions, duration, id))
         }
     }).catch(function (error: any) {
-        console.log(error)
         setError(error.response.data)
     })
+
+    function getInformationCard() {
+        return (
+            <Table className="table" id="attractionInfoCard">
+                <tbody>
+                    <tr>
+                        <th className="info">Themepark: </th>
+                        <td>{attraction?.themepark}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Opening Date: </th>
+                        <td>{attraction?.openingdate ? attraction?.openingdate : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Builder: </th>
+                        <td>{attraction?.builder ? attraction?.builder : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Type: </th>
+                        <td>{attraction?.type ? attraction?.type : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Height: </th>
+                        <td>{attraction?.height ? attraction?.height : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Length: </th>
+                        <td>{attraction?.length ? attraction?.length : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Inversions: </th>
+                        <td>{attraction?.inversions ? attraction?.inversions  : "No information found"}</td>
+                    </tr>
+                    <tr>
+                        <th className="info">Duration: </th>
+                        <td>{attraction?.duration ? attraction?.duration : "No information found"}</td>
+                    </tr>
+                </tbody>
+
+
+            </Table>
+        )
+    }
     return (
-        <div>
-            {error ? error : JSON.stringify(attraction)}
+        <div className="AttractionPage">
+            <h1>{attraction?.name}</h1>
+            {getInformationCard()}
         </div>)
 
 }
