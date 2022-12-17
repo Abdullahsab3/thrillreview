6d04bb41512a992779d3677a68914ac94195da38
Jolien Andries <jolien1.andries@gmail.com>
2022-12-17 01:26:15 +0100

    attractions werkt wel als ik gwn geen extra get request doe (vr rating en images)

--- client/src/attractions/browseAttractions.tsx
+++ client/src/attractions/browseAttractions.tsx
@@ -6,37 +6,71 @@ import { Card, ListGroup, Button, InputGroup, Form } from 'react-bootstrap';
 import StarRating from "./starRating";
 import { Search } from 'react-bootstrap-icons';
 import "../styling/browsingPage.css";
-import { ErrorCard, LoadingCard} from '../higherOrderComponents/generalCardsForBrowsing';
+import { ErrorCard, LoadingCard } from '../higherOrderComponents/generalCardsForBrowsing';
 
 
 interface attractionPreviewInterface {
+    attractionInfo: AttractionPreviewInfoInterface,
+    key: number,
+    refs?: (e: HTMLDivElement) => void,
+}
+
+interface AttractionPreviewInfoInterface {
     id: number,
     name: string,
     themepark: string,
-    img: string,
-    starrating: number,
-    key:number,
-    ref?: (e: HTMLDivElement) => void,
+    img?: string,
+    starrating?: number,
 }
 
 function AttractionPreviewCard(props: attractionPreviewInterface) {
-    return (
-        <Card className="browsingCard">
-            <Card.Title>{props.name}</Card.Title>
-            <Card.Img variant="bottom" src={props.img} alt={`picture of attraction with name ${props.name}`} />
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>Theme park: {props.themepark}</ListGroup.Item>
-                <ListGroup.Item>Rating: <StarRating rating={props.starrating} /></ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-                <Link to={`/Attractions/${props.id}`}>
-                    <Button>
-                        Go to attraction!
-                    </Button>
-                </Link>
-            </Card.Body>
-        </Card>
-    );
+    const attractionProp = props.attractionInfo
+    if (props.refs) {
+        return (
+            <Card className="browsingCard" ref={props.refs}>
+                <Card.Title>{attractionProp.name}</Card.Title>
+                {attractionProp.img ? <Card.Img variant="bottom" src={attractionProp.img} alt={`picture of attraction with name ${attractionProp.name}`} /> : ""}
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Theme park: {attractionProp.themepark}</ListGroup.Item>
+                    <ListGroup.Item>Rating: <StarRating rating={2} /></ListGroup.Item>{attractionProp.starrating}
+                </ListGroup>
+                <Card.Body>
+                    <Link to={`/Attractions/${attractionProp.id}`}>
+                        <Button>
+                            Go to attraction!
+                        </Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        );
+    } else {
+        return (
+            <Card className="browsingCard">
+                <Card.Title>{attractionProp.name}</Card.Title>
+                {attractionProp.img ? <Card.Img variant="bottom" src={attractionProp.img} alt={`picture of attraction with name ${attractionProp.name}`} /> : ""}
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Theme park: {attractionProp.themepark}</ListGroup.Item>
+                    <ListGroup.Item>Rating: <StarRating rating={2} /></ListGroup.Item>{attractionProp.starrating}
+                </ListGroup>
+                <Card.Body>
+                    <Link to={`/Attractions/${attractionProp.id}`}>
+                        <Button>
+                            Go to attraction!
+                        </Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+function isIdInArray(a: AttractionPreviewInfoInterface[], i: number): Boolean {
+    let res = false;
+    a.forEach(t => {
+        if (t.id === i) res = true;
+    });
+    console.log("id:", i, "a:", a, "res", res)
+    return res;
 }
 
 // took inspiration from https://www.youtube.com/watch?v=NZKUirTtxcg
@@ -44,38 +78,83 @@ function GetAttractions(query: string, pageNr: number) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [hasMore, setHasMore] = useState(false);
-    const [attractions, setAttractions] = useState<Attraction[]>([]);
+    const [attractions, setAttractions] = useState<AttractionPreviewInfoInterface[]>([]);
+    const LIMIT_RETURNS = 6;
 
     // to set attractions to empty
     useEffect(() => {
         setAttractions([]);
-    }, [query])
+    }, [query]);
     // to load new attractions
+    // /attractions/find
     useEffect(() => {
         setLoading(true)
         setError(false)
-        let cancel: Canceler
-        axios({
-            method: 'get',
-            url: "HIER MOET DE URL KOMEN - BACKEND",
-            params: { query: query, pagenr: pageNr },
-            cancelToken: new axios.CancelToken(c => cancel = c)
-        }).then(res => {
-            setAttractions(prevAttractions => {
-                return [...prevAttractions, ...res.data.attractions.map((a: any) => { // Attraction evt andere naam (afh v response) + DIE ANY MOET IETS ANDERS ZIJN, WSS JSON, Q AAN SERVER
-                    const { name, themepark, openingdate, builder, type, height, length, inversions, duration, id } = a
-                    new Attraction(name, themepark, openingdate, builder, type, height, length, inversions, duration, id)
-                })]
-            })
-            setHasMore(res.data.docs.lenght > 0);
-            setLoading(false)
-            console.log(res.data);
+        axios.get(`/attractions/find?query=${query}&page=${pageNr}&limit=${LIMIT_RETURNS}`).then(res => {
+            console.log("res:", res);
+
+            res.data.result.map((attr: any, i: number) => {
+                const { name, themepark, id } = attr
+
+                //   axios.get(`/attraction/${id}/rating`).then(starRes => {
+                let prevAttractions = attractions;
+                if ((pageNr <= 1) && (i <= 1)) {
+                    prevAttractions = []
+                }
+
+                console.log("id not in array,", id, prevAttractions)
+                //    const stars = starRes.data.rating
+                //   console.log("starres", stars);
+                // axios.get(`/attractions/${id}/photos`).then(imgRes => {
+                //     console.log("imgres:", imgRes);
+                // const image = "blabla"
+                if (!isIdInArray(prevAttractions, id)) {
+                    const attrInfo = {
+                        id: id,
+                        name: name,
+                        themepark: themepark,
+                        // img: image,
+                        //     starrating: stars,
+                    }
+                    prevAttractions.push(attrInfo);
+                }
+                setAttractions(prevAttractions);
+                /*  }).catch(function (error) {
+                      console.log("ERROR GECACHED", error)
+                      if (error.response.status === 404) {
+                          console.log("show meeee")
+                          const attrInfo = {
+                              id: id,
+                              name: name,
+                              themepark: themepark,
+                              starrating: stars,
+                          }
+                          prevAttractions.push(attrInfo);
+                      } else console.log("err msg", error.message);
+                  });*/
+
+
+                //    });
+
+
+            });
+
+            /*  let prevThemeparks = themeparks;
+              if (pageNr <= 1) {
+                  prevThemeparks = []
+              }
+              res.data.result.map((park: any) => {
+                  const { name, openingdate, country, street, postalcode, streetnumber, type, website, id } = park
+                  if (!isIdInArray(prevThemeparks, id))
+                  prevThemeparks.push(new ThemePark(name, openingdate, street, streetnumber, postalcode, country, type, website, id));
+              });
+              setThemeParks(prevThemeparks); */
+            setHasMore(res.data.result.length === LIMIT_RETURNS);
+            setLoading(false);
         }).catch(e => {
-            if (axios.isCancel(e)) return // if cancelled, it was meant to
             setLoading(false)
             setError(true);
         })
-        return () => cancel();
     }, [query, pageNr]);
 
     return (
@@ -84,31 +163,27 @@ function GetAttractions(query: string, pageNr: number) {
 }
 
 function BrowseAttractions() {
-    const { initialQuery } = useParams()
-    console.log(initialQuery)
+    //const { initialQuery } = useParams()
+    //console.log(initialQuery)
     const [query, setQuery] = useState("")
     const [intermediateQuery, setIntermediateQuery] = useState("")
     const [pageNr, setPageNr] = useState(1);
-    if (initialQuery) setQuery(initialQuery)
+    // if (initialQuery) setQuery(initialQuery)
     let { attractions, hasMore, loading, error } = GetAttractions(query, pageNr);
-    // HIER EEN PROBLEEM HEB AL VEEL GEKEKEN MAAR VIND HET NIET
     const observer = useRef<IntersectionObserver | null>(null);  // zonder de null (in type en in haakjes) werkte het niet, dit werkte ook niet : useRef() as React.MutableRefObject<HTMLDivElement>; 
     const lastAttractionRef = useCallback((node: HTMLDivElement) => {
-        if (loading) return // otherwise will keep sending callbacks while loading
-        console.log(node)
-        // DIT HIER GEEFT MIJ FOUTEN, MAAR IS WAT JE MOET DOEN MET SERVER ANTW DUS KAN HET NOG NIET DOEN
+        if (loading) return; // otherwise will keep sending callbacks while loading
+        console.log("node", node)
         // https://github.com/WebDevSimplified/React-Infinite-Scrolling/blob/master/src/App.js 
-            if (observer.current) observer.current.disconnect(); // disconnect current observer to connect a new one
-          observer.current = new IntersectionObserver(entries => {
-              if (entries[0].isIntersecting && hasMore) { // ref is showing on the page + there is still more
-                  setPageNr(prevPageNr => prevPageNr + 1)
-              }
-          })
-          if (node) observer.current.observe(node) 
+        if (observer.current) observer.current.disconnect(); // disconnect current observer to connect a new one
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) { // ref is showing on the page + there is still more
+                setPageNr(prevPageNr => prevPageNr + 1)
+            }
+        })
+        if (node) observer.current.observe(node)
     }, [loading, hasMore])
 
-    //  OM TE TESTEN ZONDER BACKEND
-    attractions = [new Attraction("anubis", "plopsa", "29/01/2003", "someone", "sth", "18.5", "13.5", "5", "99:59:59", 1)]
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         setPageNr(1);
@@ -116,10 +191,6 @@ function BrowseAttractions() {
         event.preventDefault()
     }
 
-    //Q : zou ik iedere keer opnieuw laten linken zodat de query update in de link?
-    //  <Link to={`/browse-attractions/${query}`}>   </Link> rond submit knop
-    // nadeel: elke keer via routing
-    // voordeel: link wordt geupdatet
     return (
         <>
             <Card className="browsingCard">
@@ -139,21 +210,22 @@ function BrowseAttractions() {
                 </Card.Body>
             </Card>
 
-            {attractions.map((attraction: Attraction, i: number) => {
+            {attractions.map((attraction: AttractionPreviewInfoInterface, i: number) => {
+                console.log("IN MAP", attraction);
                 if (attractions.length === i + 1) {
                     // HET KAN ZIJN DAT DE REF NIET WERKT, (zie error in console log, maar is v react router dom en ref is v react, dus idk - kan niet testen want moet dan iets v backend krijgen)
                     return (
-                        <AttractionPreviewCard ref={lastAttractionRef} key={attraction.id} id={attraction.id} name={attraction.name} themepark={attraction.themepark} img="https://unsplash.com/photos/C4sxVxcXEQg" starrating={3} />
+                        <AttractionPreviewCard refs={lastAttractionRef} key={attraction.id} attractionInfo={attraction} />
                     );
                 } else {
-                    return(
-                        <AttractionPreviewCard key={attraction.id} id={attraction.id} name={attraction.name} themepark={attraction.themepark} img="https://unsplash.com/photos/C4sxVxcXEQg" starrating={3} />
+                    return (
+                        <AttractionPreviewCard key={attraction.id} attractionInfo={attraction} />
                     );
                 }
             })}
-            {loading ? <LoadingCard topic={"attractions"}/> : ""}
-            {error ? <ErrorCard topic={"attractions"}/> : ""}
-           
+            {loading ? <LoadingCard topic={"attractions"} /> : ""}
+            {error ? <ErrorCard topic={"attractions"} /> : ""}
+
         </>
     );
 
--- client/src/higherOrderComponents/generalCardsForBrowsing.tsx
+++ client/src/higherOrderComponents/generalCardsForBrowsing.tsx
@@ -7,7 +7,7 @@ interface browsingInfoCardInterface{
 export function LoadingCard(props: browsingInfoCardInterface) {
     return (
         <Card>
-            <Card.Title> We are loading the ${props.topic}, please wait</Card.Title>
+            <Card.Title> We are loading the {props.topic}, please wait</Card.Title>
             <Card.Body> In the mean time, grab some tea! </Card.Body>
         </Card>
     );
@@ -16,7 +16,7 @@ export function LoadingCard(props: browsingInfoCardInterface) {
 export function ErrorCard(props: browsingInfoCardInterface) {
     return (
         <Card bg="danger" className="browsingCard mb-2" >
-            <Card.Title> There has been a problem loading the ${props.topic}. Please try again.</Card.Title>
+            <Card.Title> There has been a problem loading the {props.topic}. Please try again.</Card.Title>
             <Card.Body> Our apologies for the inconvenience. </Card.Body>
         </Card>
     );
